019833afb8a0d227c679ed927dc40fd8024c7b4e
Muhammad Shahwal <sawalrever23@gmail.com>
2025-05-04 12:07:49 +0700

    refactor: update missing deployment content

--- src/stores/projectStore.js
+++ src/stores/projectStore.js
@@ -1,5 +1,11 @@
 import { defineStore } from 'pinia'
 
+import bebastukarImage from '@/assets/images/projects/bebastukar.png'
+import recythingImage from '@/assets/images/projects/recything.png'
+import planitoriumImage from '@/assets/images/projects/planitorium.png'
+import sentimentAnalysisImage from '@/assets/images/projects/did-a-analisis.png'
+import portfolioImage from '@/assets/images/projects/portfolio.png'
+
 export const useProjectStore = defineStore('project', {
     state: () => ({
         projects: [
@@ -7,7 +13,7 @@ export const useProjectStore = defineStore('project', {
                 id: 1,
                 title: "Bebastukar - Backend",
                 description: "Developed the backend for Bebastukar, a secondhand exchange platform with user registration, community discussions, item listings, and AI chatbot assistance.",
-                image: "/src/assets/images/projects/bebastukar.png",
+                image: bebastukarImage,
                 tags: ["Go", "Echo", "Cloudinary", "MySQL", "Docker", "Generative AI"],
                 link: "https://github.com/sawalreverr/bebastukar-be"
             },
@@ -15,7 +21,7 @@ export const useProjectStore = defineStore('project', {
                 id: 2,
                 title: "RecyThing - Backend",
                 description: "Developed the backend for Recything, a mobile app enabling recycling education, litter reporting, and gamified rewards, paired with a web dashboard for admin data management.",
-                image: "/src/assets/images/projects/recything.png",
+                image: recythingImage,
                 tags: ["Go", "Echo", "Cloudinary", "EC2", "MySQL", "Docker", "OpenAI"],
                 link: "https://github.com/RECYTHNG/recything-be"
             },
@@ -23,7 +29,7 @@ export const useProjectStore = defineStore('project', {
                 id: 3,
                 title: "Planitorium - Mobile App",
                 description: "A mobile application for efficient activity planning and corn disease detection.",
-                image: "/src/assets/images/projects/planitorium.png",
+                image: planitoriumImage,
                 tags: ["Kotlin", "Android", "MVVM", "Figma"],
                 link: "https://github.com/Planitorium/planitorium-md"
             },
@@ -31,7 +37,7 @@ export const useProjectStore = defineStore('project', {
                 id: 4,
                 title: "Sentiment Analysis IKN",
                 description: "Developed a sentiment analysis system using IndoBERT and XGBoost to analyze and classify public sentiment toward Indonesia's new capital city (IKN) from X platform data, deployed via an interactive Streamlit dashboard.",
-                image: "/src/assets/images/projects/did-a-analisis.png",
+                image: sentimentAnalysisImage,
                 tags: ["Python", "Streamlit", "Indobert", "XGBoost"],
                 link: "https://github.com/sawalreverr/did-a-analisis",
             },
@@ -39,7 +45,7 @@ export const useProjectStore = defineStore('project', {
                 id: 5,
                 title: "Portfolio Website",
                 description: "A personal portfolio website showcasing my projects.",
-                image: "/src/assets/images/projects/portfolio.png",
+                image: portfolioImage,
                 tags: ["Vue", "Tailwind CSS", "DaisyUI"],
                 link: "https://github.com/sawalreverr/did-a-analisis",
             },
